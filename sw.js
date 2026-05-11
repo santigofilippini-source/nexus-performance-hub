48aec0186d7d0872392b2610657f75d9ff4735c0
Santiago Filippini <santigofilippini@gmail.com>
2026-05-11 00:47:18 -0300

    Add PWA support: manifest and service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'qoore-v1';
+const ASSETS = ['/', '/index.html', '/app.js', '/style.css'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
